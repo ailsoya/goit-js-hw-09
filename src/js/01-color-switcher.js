0d6efaca9726fa8638abdf029bcc7fe30e9ed8a2
ailsoya <akalitata53@gmail.com>
2023-08-20 23:39:53 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const backround = document.querySelector("body")
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
 
+startButton.addEventListener('click', changingColors)
+stopButton.addEventListener('click', stopChangingColors)
+
+stopButton.disabled = true
+
+function changingColors() {
+    backround.style.backgroundColor = getRandomHexColor()
+    startButton.disabled = true
+    stopButton.disabled = false
+    timerId = setInterval(() => {
+        backround.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+
+function stopChangingColors() {
+    stopButton.disabled = true
+    startButton.disabled = false
+    clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
