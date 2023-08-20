bf4e51c802b2d1fc7093453afcc97af3aebd0732
ailsoya <130087947+ailsoya@users.noreply.github.com>
2023-08-20 20:44:05 +0000

    Deploying to gh-pages from @ ailsoya/goit-js-hw-09@0d6efaca9726fa8638abdf029bcc7fe30e9ed8a2 🚀

---
+++ 01-color-switcher.d63d6da7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(function(){t.style.backgroundColor=o(),e.disabled=!0,d.disabled=!1,timerId=setInterval((()=>{t.style.backgroundColor=o()}),1e3)})),d.addEventListener("click",(function(){d.disabled=!0,e.disabled=!1,clearInterval(timerId)})),d.disabled=!0;
+//# sourceMappingURL=01-color-switcher.d63d6da7.js.map
