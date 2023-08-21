74d5f1faa0bfa9216d27195c18f22f27f66e62b0
ailsoya <akalitata53@gmail.com>
2023-08-21 14:58:04 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,15 @@
+import Notiflix from 'notiflix'
+
+const firstDelay = document.querySelector('[name="delay"]')
+const delayStep = document.querySelector('[name="step"]')
+const amount = document.querySelector('[name="amount"]')
+const startBtn = document.querySelector('button')
+
+startBtn.addEventListener('submit', createPromise)
+
 function createPromise(position, delay) {
+  preventDefault()
+  console.log('лох')
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
