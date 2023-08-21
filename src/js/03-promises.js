f713e46dad6f543c8945862c2973633562b69dbe
ailsoya <akalitata53@gmail.com>
2023-08-21 15:32:51 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,18 +2,37 @@ import Notiflix from 'notiflix'
 
 const firstDelay = document.querySelector('[name="delay"]')
 const delayStep = document.querySelector('[name="step"]')
-const amount = document.querySelector('[name="amount"]')
-const startBtn = document.querySelector('button')
+const Amount = document.querySelector('[name="amount"]')
+const form = document.querySelector('.form')
 
-startBtn.addEventListener('submit', createPromise)
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+  let delay = Number(firstDelay.value)
+  const step = Number(delayStep.value)
+  const amount = Number(Amount.value)
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+    delay += step
+  }
+})
 
 function createPromise(position, delay) {
-  preventDefault()
-  console.log('лох')
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+    })
 }
